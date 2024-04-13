b73ac3a0c5147d848a34d33fbff3f51547511908
Lucas Reljic <lucas.reljic@gmail.com>
2024-04-13 00:15:30 -0400

    fixed navbar links and remove skillboxes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,21 @@
-import React, { Suspense } from "react";
+import React from "react";
 import styles from "./Home.module.css";
 import Text from "../components/Elements/Text/Text";
 import "react-slideshow-image/dist/styles.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Blocks from "../components/Elements/Blocks/Blocks";
 import IconBox from "../components/Elements/Icons/IconBox";
-import Button2 from "../components/Intro/Button2";
-import ReactCardFlip from "react-card-flip";
 import Appear from "../components/Intro/Appear";
-import { useState } from "react";
 import {
   Slide,
   Fade,
-  JackInTheBox,
   AttentionSeeker,
   Reveal,
 } from "react-awesome-reveal";
 
 // Photos/logos
 import arrow from "../icons/arrow-down-3101.svg";
-import ReactSVG from "./../icons/react.svg";
 import frcPhoto from "./../icons/FRC-Vertical.png";
-import CppPhoto from "./../icons/C++_Logo.png";
 import rocketryPhoto from "./../icons/rocketry.png";
 import uwaterlooPhoto from "./../icons/University_of_Waterloo.svg.png";
 import goodlabsPhoto from "./../icons/goodlabs.png";
@@ -42,17 +36,9 @@ import piLaptop from "../components/Elements/Slide/photos/RPiLaptop/IMG_6924.jpe
 import planes from "../components/Elements/Slide/photos/RCplanes/latest1.jpeg";
 import gokart from "../components/Elements/Slide/photos/gokart/IMG_1418.jpeg";
 
-const SkillBox = React.lazy(() =>
-  import("../components/Elements/Skills/SkillBox")
-);
-
 export default function Home() {
-  const [flip, setFlip] = useState(false);
-  const [flip0, setFlip0] = useState(false);
-  const [flip2, setFlip2] = useState(false);
-  const [flip3, setFlip3] = useState(false);
   const handleClickScroll = () => {
-    const element = document.getElementById("skills");
+    const element = document.getElementById("projects");
     if (element) {
       // Will scroll smoothly to the top of the next section
       element.scrollIntoView({ behavior: "smooth" });
@@ -131,153 +117,7 @@ export default function Home() {
         </Slide>
       </Appear>
       <AttentionSeeker effect="pulse" duration={1000} delay={1000}>
-        <h1 id="skills" className={styles.skills}>
-          Programming Languages I know
-        </h1>
-      </AttentionSeeker>
-      <Row lg={2} md={1} sm={1} xs={1}>
-        <Col>
-          <Reveal delay={100} triggerOnce={true} style={{ opacity: "0" }}>
-            <Slide delay={0} duration={2000} triggerOnce={true}>
-              <ReactCardFlip isFlipped={flip} flipDirection="vertical">
-                <Suspense fallback={<div>Loading...</div>}>
-                  <SkillBox
-                    header="3 YEARS"
-                    click={() => setFlip(!flip)}
-                    color={
-                      "-25deg, #06257a 0%, #0054c2 35%, rgb(241, 58, 2) 100%"
-                    }
-                    className={styles.javaIcon}
-                    url={"https://www.svgrepo.com/show/184143/java.svg"}
-                  >
-                    Java
-                  </SkillBox>
-                </Suspense>
-                <Button2
-                  left="10rem"
-                  link={() => setFlip(!flip)}
-                  color={
-                    "-145deg, #06257a 0%, #0054c2 35%, rgb(241, 58, 2) 100%"
-                  }
-                >
-                  During my five-year involvement in FRC, Java was the language
-                  for programming the robots. This extensive hands-on experience
-                  enabled me to gain a deep understanding of Java's syntax,
-                  features, and best practices. Applying Java in real-world FRC
-                  scenarios enhanced my problem-solving skills and enabled me to
-                  develop robust, efficient, and reliable solutions to complex
-                  challenges.
-                </Button2>
-              </ReactCardFlip>
-            </Slide>
-          </Reveal>
-        </Col>
-        <Col>
-          <Reveal delay={0} triggerOnce={true} style={{ opacity: "0" }}>
-            <Slide
-              direction="right"
-              delay={0}
-              duration={2000}
-              triggerOnce={true}
-            >
-              <ReactCardFlip isFlipped={flip0} flipDirection="vertical">
-                <SkillBox
-                  header="2 YEARS"
-                  click={() => setFlip0(!flip0)}
-                  color={
-                    "155deg, #06257a 0%, #0054c2 35%, rgb(255, 222, 76) 100%"
-                  }
-                  className={styles.pythonIcon}
-                  url={
-                    "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
-                  }
-                >
-                  Python
-                </SkillBox>
-
-                <Button2
-                  color={
-                    "35deg, #06257a 0%, #0054c2 35%, rgb(255, 222, 76) 100%"
-                  }
-                  link={() => setFlip0(!flip0)}
-                >
-                  As a part of FRC, I gained extensive experience in Python
-                  programming, specifically in vision systems. Utilizing
-                  Python's libraries and tools, I implemented efficient
-                  solutions for image processing and analysis. This hands-on
-                  experience enhanced my proficiency in Python's syntax, data
-                  structures, and built-in functions, which I further expanded
-                  through experiences like the Toyota Challenge.
-                </Button2>
-              </ReactCardFlip>
-            </Slide>
-          </Reveal>
-        </Col>
-        <Col>
-          <Reveal delay={0} triggerOnce={true} style={{ opacity: "0" }}>
-            <Slide
-              direction="right"
-              delay={100}
-              duration={2000}
-              triggerOnce={true}
-            >
-              <ReactCardFlip isFlipped={flip2} flipDirection="vertical">
-                <SkillBox
-                  header="6 MONTHS"
-                  className={styles.cppIcon}
-                  click={() => setFlip2(!flip2)}
-                  color={
-                    "-10deg, #06557a 0%, #2626ff 35%, rgba(0, 212, 255, 1) 100%"
-                  }
-                  url={CppPhoto}
-                >
-                  C++
-                </SkillBox>
-                <Button2
-                  color={
-                    "-140deg, #06557a 0%, #2626ff 35%, rgba(0, 212, 255, 1) 100%"
-                  }
-                  link={() => setFlip2(!flip2)}
-                >
-                  In my engineering program, I gained extensive knowledge of the
-                  C++ programming language, achieving top marks in my
-                  programming classes. I also have hands-on experience with
-                  Arduino boards, utilizing C++ to program and control
-                  electronic systems. This practical application of C++ through
-                  Arduino projects has strengthened my proficiency in the
-                  language and working with hardware.
-                </Button2>
-              </ReactCardFlip>
-            </Slide>
-          </Reveal>
-        </Col>
-        <Col xs lg="6">
-          <Reveal delay={100} triggerOnce={true} style={{ opacity: "0" }}>
-            <JackInTheBox delay={0} duration={2000} triggerOnce={true}>
-              <ReactCardFlip isFlipped={flip3} flipDirection="vertical">
-                <SkillBox
-                  className={styles.Applogo}
-                  header="3 MONTH"
-                  url={ReactSVG}
-                  click={() => setFlip3(!flip3)}
-                >
-                  React JS
-                </SkillBox>
-                <Button2 link={() => setFlip3(!flip3)}>
-                  Through my involvement in Waterloo Rocketry, I gained valuable
-                  experience in React, learning its syntax, styles, and various
-                  components. Working on projects like the Rocketry website and
-                  this current website has significantly enhanced my Javascript
-                  skills and provided me with a solid foundation in React.
-                </Button2>
-              </ReactCardFlip>
-            </JackInTheBox>
-          </Reveal>
-        </Col>
-        <Col className={styles.skillText}></Col>
-      </Row>
-      <AttentionSeeker effect="pulse" duration={1000} delay={1000}>
-        <h1 className={styles.gallery}>VIEW PROJECTS</h1>
+        <h1 className={styles.gallery} id="projects">VIEW PROJECTS</h1>
       </AttentionSeeker>
       <Container className={styles.containerFluid} fluid>
         <Row xxl={4} xl={3} lg={2} md={2} sm={1} xs={1}>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { Container, NavItem } from "react-bootstrap/";
+import { Link, Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap/";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -7,35 +7,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Layout.module.css";
 import mail from "./../components/NavSocials/gmail.png";
 import github from "./../components/NavSocials/github.png";
-import { JackInTheBox, Fade } from "react-awesome-reveal";
-import Appear from "../components/Intro/Appear";
 
 import linkedIn from "./../components/NavSocials/official-linkedin-logo----17.png";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 const Layout = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  // const [isVisible, setIsVisible] = useState(true);
 
-  useEffect(() => {
-    setIsVisible(true);
-  }, []);
+  // useEffect(() => {
+  //   setIsVisible(true);
+  // }, []);
 
-  var [text, setText] = useState("");
+  // var [text, setText] = useState("");
   return (
     <>
       <Navbar
-        onPointerEnter={() => setText(Math.random() > 0.2 ? "" : "intro")}
-        bg="dark"
-        variant="dark"
+        bg="black"
+        variant="black"
         style={{ top: "0.5rem", display: "inline" }}
       >
         <Container fluid>
+        <Navbar.Brand as={Link} to="/"><span className={styles.title}>Home</span></Navbar.Brand>
           <Nav className="me-auto justify-content-center bar">
-            <NavItem className="my-auto px-auto">
-              <NavLink to=" " style={{ textDecoration: "none" }}>
-                <span className={styles.title}>Home</span>
-              </NavLink>
-            </NavItem>
             <NavDropdown
               style={{ color: "black" }}
               title={<span className={styles.dropdown}>Projects</span>}
